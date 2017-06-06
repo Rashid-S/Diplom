d670632a26b48ad24e5ed7d9c6b7967fcb8b6f8a
Rinat Khabibiev <srenskiy@gmail.com>
2017-06-06 10:07:00 +0300

    Исправления по замечаниям от 25.05.2017 (#1)
    * Исправления по замечаниям от 25.05.2017
    
    * Исправил первое замечание
    
    * Исправил второе замечание
    
    * Исправление третьего замечания
    
    * Еще раз третье замечание подправил
    
    * Кстати....убрал из кода токен
    
    * Update README.md
    
    * Кое-что поправил

--- Diplom_Netologia.py
+++ Diplom_Netologia.py
@@ -4,91 +4,103 @@ import time
 
 
 def get_params():
-    VERSION = '5.64'
+    """
+    Формироваине параметров для запросов
+    """
+    version = '5.64'
     access_token = "d13e692be69592b09fd22c77a590dd34e186e6d696daa88d6d981e1b4e296b14acb377e82dcbc81dc0f22"  # token
     params = {'access_token': access_token,
-              'v': VERSION,
+              'v': version,
               }
     return params
 
 
-def get_user_friends_id(params):
+def print_process():
+    """
+    Показ процесса работы 
+    """
     print('.')
-    response = requests.get('https://api.vk.com/method/friends.get', params)
-    friends_list = response.json()
-    return friends_list
 
 
-def get_user_groups(params):
-    print('.')
-    response = requests.get('https://api.vk.com/method/groups.get', params)
-    groups_list = response.json()
-    return groups_list
-
-
-def get_group_members_id(params):
-    print('.')
-    response = requests.get('https://api.vk.com/method/groups.getMembers', params)
-    members_list = response.json()
-    return members_list
+def make_request(url, params):
+    """
+    Отправка запроса
+    """
+    print_process()
+    attempts = 0
+    while True:
+            response = requests.get(url, params)
+            if response.ok:
+                response_list = response.json()
+                if 'response' in response_list:
+                    return response_list
+                else:
+                    attempts += 1
+                    if attempts == 3:
+                        print('END')
+                        exit()
+            time.sleep(0.5)
 
 
 def get_users_is_members(params, friends_list):
+    """
+    Является ли пользователь из списка друзей подписчиком группы
+    """
     final_list = []
-    if len(params['user_ids'].split(', ')) > 250:
-        x = 0
-        y = 250
-        all_params = params['user_ids'].split(', ')
-        while all_params[x:y]:
-            params['user_ids'] = str(friends_list['response']['items'][x:y])[1:-1]
-            x = y
-            y += 250
-            # time.sleep(1)
-            print('.')
-            response = requests.get('https://api.vk.com/method/groups.isMember', params)
-            is_members = response.json()
-            for member in is_members['response']:
-                final_list.append(member)
+    init_start = 0
+    pause = 250
+    list_user_ids = params['user_ids'].split(', ')
+    while list_user_ids[init_start:pause]:
+        params['user_ids'] = str(friends_list['response']['items'][init_start:pause])[1:-1]
+        init_start = pause
+        pause += 250
+        members = make_request('https://api.vk.com/method/groups.isMember', params)
+        for member in members['response']:
+            final_list.append(member)
     return final_list
 
 
 def get_group_without_user_friends(params, groups_list, friends_list):
-    user_is_along = []
+    """
+    Формируем лист_групп пользователя в которые не входят ни один из его друзей 
+    """
+    user_alone = []
     for group in groups_list['response']['items']:
         params['group_id'] = group['id']
         params['user_ids'] = str(friends_list['response']['items'])[1:-1]
         members_group = get_users_is_members(params, friends_list)
+        flag = False  # установка флага по умолчанию Не истина
         for member in members_group:
-            flag = False
             if member['member'] == 1:
                 flag = True
                 break
         if not flag:
             params['group_id'] = group['id']
-            try:
-                str_dict = {'name': group['name'],
-                            'gid': group['id'],
-                            'members_count': get_group_members_id(params)['response']['count']}
-            except:
-                print('except')
-            user_is_along.append(str_dict)
-        time.sleep(1)
-    return user_is_along
+            str_dict = {'name': group['name'], 'gid': group['id'],
+                        'members_count': make_request('https://api.vk.com/method/groups.getMembers',
+                                                      params)['response']['count']}
+            user_alone.append(str_dict)
+    return user_alone
 
 
 def save_json(data):
+    """
+    Сохранение файла в json
+    """
     with open('result.json', 'w', encoding='utf-8') as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
+        print(len(data))
 
 
 def main():
     params = get_params()
     params['user_id'] = 5030613
-    friends_list = get_user_friends_id(params)
+    friends_list = make_request('https://api.vk.com/method/friends.get', params)
     params['extended'] = 1
-    groups_list = get_user_groups(params)
-
+    groups_list = make_request('https://api.vk.com/method/groups.get', params)
     data = get_group_without_user_friends(params, groups_list, friends_list)
     save_json(data)
 
-main()
+
+if __name__ == '__main__':
+    main()
